663b17d5d7360eb0d7c42bfba87465f82ff5c05d
BeniceBB <info@benicebot.nl>
2022-01-14 13:54:45 +0100

    Added testBlog test

--- tests/Entity/BlogTest.php
+++ tests/Entity/BlogTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Entity;
 
 use App\Entity\Blog;
+use App\Entity\User;
 use PHPUnit\Framework\TestCase;
 
 class BlogTest extends TestCase
@@ -10,8 +11,13 @@ class BlogTest extends TestCase
     public function testAdd(): void
     {
         $blog = new Blog();
-
-        $this->assertSame(null, $blog->getId());
+        $this->assertNull($blog->getId());
     }
 
+    public function testBlog(): void
+    {
+        $stub = $this->createMock(Blog::class);
+        $stub->method('getUser')->willReturn(new User());
+        $this->assertEquals(new User(), $stub->getUser());
+    }
 }
