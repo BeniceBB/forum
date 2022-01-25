6b18a9cb3c75c110188b80aa77cb138b410e423b
BeniceBB <info@benicebot.nl>
2022-01-25 16:29:42 +0100

    started test

--- tests/Service/BlogListTest.php
+++ tests/Service/BlogListTest.php
@@ -3,14 +3,18 @@
 
 namespace App\Tests\Service;
 
+use App\Services\BlogListManager;
 use PHPUnit\Framework\TestCase;
 
 class BlogListTest extends TestCase
 {
-    public function testCheckByPostType(): bool
+    public function testCheckByPostType(): void
     {
-//        $co
-        return true;
+       $newTest = $this->createMock(BlogListManager::class);
+       $newTest->method('getFilters');
+       $newTest->getFilteredBlogs();
+       dump($newTest);exit;
+       static::assertSame([], $newTest);
     }
 
 }
